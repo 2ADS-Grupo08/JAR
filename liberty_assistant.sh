6b4397d7aad7fb928c005fcf094e5d66da13a12d
ErykaMML <eryka.lopes@sptech.school>
2023-05-27 12:36:37 -0300

    Estilizando o assistente de instalação

--- liberty_assistant.sh
+++ liberty_assistant.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
+CIANO='\033[0;36m'
+NC='\033[0m'
 clear
-echo " _     _ _               _            ____       "      
-echo "| |   (_) |__   ___ _ __| |_ _   _   / ___|___   "  
-echo "| |   | | '_ \ / _ \ '__| __| | | | | |   / _ \  " 
-echo "| |___| | |_) |  __/ |  | |_| |_| | | |__| (_) | "
-echo "|_____|_|_.__/ \___|_|   \__|\__, |  \____\___/  " 
-echo "                             |___/               "
+echo -e "${CIANO} _     _ _               _            ____       "
+echo -e "| |   (_) |__   ___ _ __| |_ _   _   / ___|___   "
+echo -e "| |   | | '_ \ / _ \ '__| __| | | | | |   / _ \  "
+echo -e "| |___| | |_) |  __/ |  | |_| |_| | | |__| (_) | "
+echo -e "|_____|_|_.__/ \___|_|   \__|\__, |  \____\___/  "
+echo -e "                             |___/               ${NC}"
+
 
 sleep 3
 clear
 
-echo -e "[Liberty-assistant]: Olá, serei seu assistente para instalação do Java, docker e MYSQL!!!"
+echo -e "${CIANO}[Liberty-assistant]${NC}: Olá, serei seu assistente para instalação do Java, docker e MYSQL!!!"
 sleep 3
-echo -e "[Liberty-assistant]: Irei atualizar os pacotes do sistema. Só um momento!"
+echo -e "${CIANO}[Liberty-assistant]${NC}: Irei atualizar os pacotes do sistema. Só um momento!"
 sudo apt update && sudo apt upgrade –y
-echo -e "[Liberty-assistant]: Pacotes atualizados!"
+echo -e "${CIANO}[Liberty-assistant]${NC}: Pacotes atualizados!"
 clear
 sleep 2
-echo -e "[Liberty-assistant]: Verificando se você possui o Java instalado..."
+echo -e "${CIANO}[Liberty-assistant]${NC}: Verificando se você possui o Java instalado..."
 echo -e "\n"
 
 sleep 3
@@ -25,33 +28,33 @@ java -version >/dev/null 2>&1
 
 if [ $? -eq 0 ]
 	then
-		echo -e "[Liberty-assistant]: Você já tem o java instalado!!!"
+		echo -e "${CIANO}[Liberty-assistant]${NC}: Você já tem o java instalado!!!"
 		echo -e "    "
 		sleep 1
 
-		echo -e "[Liberty-assistant]: E essa é a sua atual versão:"
+		echo -e "${CIANO}[Liberty-assistant]${NC}: E essa é a sua atual versão:"
 		java -version
 		sleep 3
 		clear
 	else
-		echo -e"[Liberty-assistant]:  Opa! Não identifiquei nenhuma versão do Java instalado, mas sem problemas, irei resolver isso agora!"
-		echo -e "[Liberty-assistant]:  Confirme para se realmente deseja instalar o Java (S/N)?"
+		echo -e"${CIANO}[Liberty-assistant]${NC}:  Opa! Não identifiquei nenhuma versão do Java instalado, mas sem problemas, irei resolver isso agora!"
+		echo -e "${CIANO}[Liberty-assistant]${NC}:  Confirme para se realmente deseja instalar o Java (S/N)?"
 	read inst
 		if [ \"$inst\" == \"S\" ]
 			then
-				echo -e "[Liberty-assistant]:  Ok! Você escolheu instalar o Java"
-				echo -e "[Liberty-assistant]: Atualizando Java para a versão 17. Confirme a instalação quando solicitado."
+				echo -e "${CIANO}[Liberty-assistant]${NC}:  Ok! Você escolheu instalar o Java"
+				echo -e "${CIANO}[Liberty-assistant]${NC}: Atualizando Java para a versão 17. Confirme a instalação quando solicitado."
 				sudo apt install openjdk-17-jdk -y;
 				clear
-				echo -e "[Liberty-assistant]: Java atualizado com sucesso!"
+				echo -e "${CIANO}[Liberty-assistant]${NC}: Java atualizado com sucesso!"
 				else
-				echo -e "[Liberty-assistant]: A versão atual do Java não atende aos requisitos mínimos. Atualize o Java para a versão 17 ou superior para continuar."
+				echo -e "${CIANO}[Liberty-assistant]${NC}: A versão atual do Java não atende aos requisitos mínimos. Atualize o Java para a versão 17 ou superior para continuar."
 	fi
 fi
 
 sleep 2
 
-echo -e "[Liberty-assistant]: Verificando se você possui o Docker instalado..."
+echo -e "${CIANO}[Liberty-assistant]${NC}: Verificando se você possui o Docker instalado..."
 sleep 2
 docker --version >/dev/null 2>&1
 
@@ -59,7 +62,7 @@ if [ $? -eq 0 ]
 	then
                 echo -e "    "
                 echo -e "    "
-		echo -e "[Liberty-assistant]: Você já tem o docker instalado!!!"
+		echo -e "${CIANO}[Liberty-assistant]${NC}: Você já tem o docker instalado!!!"
                 echo -e "    "
 		sleep 1
 		sudo systemctl start docker
@@ -68,31 +71,31 @@ if [ $? -eq 0 ]
 		sleep 3
 		clear
 
-		echo -e "[Liberty-assistant]: E essa é a versão atual do docker:"
+		echo -e "${CIANO}[Liberty-assistant]${NC}: E essa é a versão atual do docker:"
 		docker --version
 	else
-		echo -e"[Liberty-assistant]:  Opa! Não identifiquei nenhuma versão do Docker instalado, mas sem problemas, irei resolver isso agora!"
-		echo -e "[Liberty-assistant]:  Confirme para se realmente deseja instalar o Java (S/N)?"
+		echo -e "${CIANO}[Liberty-assistant]${NC}:  Opa! Não identifiquei nenhuma versão do Docker instalado, mas sem problemas, irei resolver isso agora!"
+		echo -e "${CIANO}[Liberty-assistant]${NC}:  Confirme para se realmente deseja instalar o Java (S/N)?"
 	read inst
 		if [ \"$inst\" == \"S\" ]
 			then
-				echo -e "[Liberty-assistant]:  Ok! Você escolheu instalar o Docker"
-				echo -e "[Liberty-assistant]:  Realizando download!"
+				echo -e "${CIANO}[Liberty-assistant]${NC}:  Ok! Você escolheu instalar o Docker"
+				echo -e "${CIANO}[Liberty-assistant]${NC}:  Realizando download!"
 				sleep 2
 				sudo apt install docker.io -y				
 				echo -e "\n"
-				echo -e "[Liberty-assistant]:  Atualizando! Quase lá."
+				echo -e "${CIANO}[Liberty-assistant]${NC}:  Atualizando! Quase lá."
 				sleep 2
 				echo -e "\n"
 				sudo apt update && sudo apt upgrade –y
 		                echo -e "    "
 		                echo -e "    "
-				echo -e "[Liberty-assistant]: Obrigado pela paciência! Aproveite nossas ferramentas!"
+				echo -e "${CIANO}[Liberty-assistant]${NC}: Obrigado pela paciência! Aproveite nossas ferramentas!"
 		fi
 fi
 echo -e "    "
 echo -e "    "
-echo -e "[Liberty-assistant]: Verificando se você possui o MySQL instalado..."
+echo -e "${CIANO}[Liberty-assistant]${NC}: Verificando se você possui o MySQL instalado..."
 echo -e "    "
 
 sleep 2
@@ -100,48 +103,48 @@ sudo docker images | grep mysql >/dev/null 2>&1
 
 if [ $? -eq 0 ]
         then
-                echo -e "[Liberty-assistant]: Você já tem o MySQL instalado!!!"
+                echo -e "${CIANO}[Liberty-assistant]${NC}: Você já tem o MySQL instalado!!!"
 		echo -e "    "
-                echo -e "[Liberty-assistant]: A versão da imagem SQL é 5.7"
+                echo -e "${CIANO}[Liberty-assistant]${NC}: A versão da imagem SQL é 5.7"
  		sleep 3
  		clear
         else
-                echo -e"[Liberty-assistant]: Opa! Não identifiquei nenhuma versão da imagem do MySQL instalado, mas sem problemas, irei resolver isso agora!"
-                echo -e "[Liberty-assistant]: Confirme para se realmente deseja instalar a imagem MySQL (S/N)?"
+                echo -e "${CIANO}[Liberty-assistant]${NC}: Opa! Não identifiquei nenhuma versão da imagem do MySQL instalado, mas sem problemas, irei resolver isso agora!"
+                echo -e "${CIANO}[Liberty-assistant]${NC}: Confirme para se realmente deseja instalar a imagem MySQL (S/N)?"
         read inst
                 if [ \"$inst\" == \"S\" ]
                         then
-                                echo -e "[Liberty-assistant]: Ok! Você escolheu instalar a imagem MySQL"
-                                echo -e "[Liberty-assistant]: Realizando download!"
+                                echo -e "${CIANO}[Liberty-assistant]${NC}: Ok! Você escolheu instalar a imagem MySQL"
+                                echo -e "${CIANO}[Liberty-assistant]${NC}: Realizando download!"
                                 sleep 2
 				sudo docker pull mysql:5.7
                                 echo -e "\n"
-                                echo -e "[Liberty-assistant]: Atualizando! Quase lá."
+                                echo -e "${CIANO}[Liberty-assistant]${NC}: Atualizando! Quase lá."
                                 sleep 2
                                 echo -e "\n"
                                 sudo apt update && sudo apt upgrade –y
-                                echo -e "[Liberty-assistant]: Obrigado pela paciência! Aproveite nossas ferramentas!"
+                                echo -e "${CIANO}[Liberty-assistant]${NC}: Obrigado pela paciência! Aproveite nossas ferramentas!"
                                 sleep 3
                                 clear
                 fi
 fi
 	sleep 3
-	echo -e "[Liberty-assistant]: Agora que fora realizado a instalação de todas as ferramentas, iremos criar o ambiente!"
-	echo -e "[Liberty-assistant]: Vamos criar uma imagem do banco de dados MYSQL."
+	echo -e "${CIANO}[Liberty-assistant]${NC}: Agora que fora realizado a instalação de todas as ferramentas, iremos criar o ambiente!"
+	echo -e "${CIANO}[Liberty-assistant]${NC}: Vamos criar uma imagem do banco de dados MYSQL."
         echo -e "    "
         echo -e "    "
 	sleep 1
 
 if [ $( sudo docker ps -a | grep liberty-co | wc -l ) -gt 0 ];
 	then
-		echo -e "[Liberty-assistant]: Você já possuí o banco na sua máquina! Vamos inicializar esse container"
+		echo -e "${CIANO}[Liberty-assistant]${NC}: Você já possuí o banco na sua máquina! Vamos inicializar esse container"
 		sleep 2
 				sudo docker start liberty-co >/dev/null 2>&1
 		sleep 2
-		echo -e "[Libert-assistant]: Inicializado container Liberty!"
+		echo -e "${CIANO}[Libert-assistant]${NC}: Inicializado container Liberty!"
 
 	else
-		echo -e "[Liberty-assistant]: Não encontramos nenhum banco do mysql na sua máquina, vamos resolver isso!"
+		echo -e "${CIANO}[Liberty-assistant]${NC}: Não encontramos nenhum banco do mysql na sua máquina, vamos resolver isso!"
 		sleep 2
 		sudo docker run -d -p 3306:3306 --name liberty-co -e "MYSQL_DATABASE=liberty-co" -e "MYSQL_ROOT_PASSWORD=#Gfgrupo8" mysql:5.7
 		sleep 10
@@ -208,26 +211,26 @@ CREATE TABLE IF NOT EXISTS Log (
 
         echo -e "    "
         echo -e "    "
-echo -e "[Liberty-assistant]: Agora foram criados os bancos em sua máquina, o monitoramento será iniciado após a instalação de nossa aplicação! Peço que aguarde a instalação!"
+echo -e "${CIANO}[Liberty-assistant]${NC}: Agora foram criados os bancos em sua máquina, o monitoramento será iniciado após a instalação de nossa aplicação! Peço que aguarde a instalação!"
         echo -e "    "
         echo -e "    "
 sleep 3
 
 if [ -f "captura-componentes-liberty-co-1.0-SNAPSHOT-jar-with-dependencies.jar" ];
 	then
-		echo -e "Visto que já possuí o arquivo baixado, não iremos baixar novamente"
+		echo -e "${CIANO}[Liberty-assistant]${NC}:Visto que já possuí o arquivo baixado, não iremos baixar novamente"
 		sleep 2
 	else
 		wget https://github.com/2ADS-Grupo08/JAR/raw/main/captura-componentes-liberty-co/target/captura-componentes-liberty-co-1.0-SNAPSHOT-jar-with-dependencies.jar
-		echo -e "Baixado o arquivo, aguarde para execução!"
+		echo -e "${CIANO}[Liberty-assistant]${NC}:Baixado o arquivo, aguarde para execução!"
 		echo -e "    "
 		echo -e "    "
 		sleep 2
 fi
-echo -e "[Liberty-assistant]: As dependencias foram baixados, iremos executar e peço que não encerre sua máquina"
+echo -e "${CIANO}[Liberty-assistant]${NC}: As dependencias foram baixados, iremos executar e peço que não encerre sua máquina"
 
 java -jar captura-componentes-liberty-co-1.0-SNAPSHOT-jar-with-dependencies.jar
 
-echo -e "[Liberty-assistant]: Obrigado por instalar nossa solução, peço que não encerre a aba de monitoramento! A Liberty Company agradece.
+echo -e "${CIANO}[Liberty-assistant]${NC}: Obrigado por instalar nossa solução, peço que não encerre a aba de monitoramento! A Liberty Company agradece.
 
 
